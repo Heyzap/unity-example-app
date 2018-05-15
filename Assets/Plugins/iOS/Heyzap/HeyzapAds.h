6ed6a1aa5745698cdc32c0b93ba3b800b566c73a
Rafael Leão <rafael.leao@fyber.com>
2018-05-15 12:25:47 +0200

    Update iOS static library

--- Assets/Plugins/iOS/Heyzap/HeyzapAds.h
+++ Assets/Plugins/iOS/Heyzap/HeyzapAds.h
@@ -61,7 +61,7 @@
 #define NS_ENUM(_type, _name) enum _name : _type _name; enum _name : _type
 #endif
 
-#define SDK_VERSION @"9.19.1"
+#define SDK_VERSION @"9.20.0"
 
 #if __has_feature(objc_modules)
 @import AdSupport;
